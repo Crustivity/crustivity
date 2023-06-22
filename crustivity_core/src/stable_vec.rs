8629c38993eb8d3fb3f8f8ec1174203c2e047c53
N4tus <33029422+N4tus@users.noreply.github.com>
2023-06-22 13:09:23 +0200

    Experiment with removable item from stable vec

--- crustivity_core/src/stable_vec.rs
+++ crustivity_core/src/stable_vec.rs
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 use std::mem::MaybeUninit;
 
 const BUCKET_SIZE: usize = 1 << 8;
@@ -28,10 +33,46 @@ impl<T> Bucket<T> {
     }
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+enum AssociationState {
+    Full,
+    Hollow,
+    Disabled,
+}
+
+impl AssociationState {
+    fn get_ascci_byte(self) -> u8 {
+        match self {
+            AssociationState::Full => b'F',
+            AssociationState::Hollow => b'H',
+            AssociationState::Disabled => b'D',
+        }
+    }
+}
+
 #[derive(Clone, Copy)]
 struct Association {
     bucket_idx: usize,
-    full: bool,
+    state: AssociationState,
+}
+
+trait AssociationExt {
+    fn get_valid(&self, index: usize) -> Option<&Association>;
+    fn mut_valid(&mut self, index: usize) -> Option<&mut Association>;
+}
+
+impl AssociationExt for Vec<Association> {
+    fn get_valid(&self, index: usize) -> Option<&Association> {
+        self.get(index).and_then(|association| {
+            (association.state != AssociationState::Disabled).then_some(association)
+        })
+    }
+
+    fn mut_valid(&mut self, index: usize) -> Option<&mut Association> {
+        self.get_mut(index).and_then(|association| {
+            (association.state != AssociationState::Disabled).then_some(association)
+        })
+    }
 }
 
 pub(crate) struct SVec<T> {
@@ -48,7 +89,7 @@ impl<T> SVec<T> {
     }
 
     pub(crate) fn get_mut(&mut self, index: usize) -> Option<&mut T> {
-        let association = *self.association.get(index / BUCKET_SIZE)?;
+        let association = *self.association.get_valid(index / BUCKET_SIZE)?;
 
         let bucket = self.data.get_mut(association.bucket_idx)?;
 
@@ -61,7 +102,7 @@ impl<T> SVec<T> {
     }
 
     pub(crate) fn get(&self, index: usize) -> Option<&T> {
-        let association = *self.association.get(index / BUCKET_SIZE)?;
+        let association = *self.association.get_valid(index / BUCKET_SIZE)?;
 
         let bucket = self.data.get(association.bucket_idx)?;
 
@@ -74,7 +115,10 @@ impl<T> SVec<T> {
     }
 
     pub(crate) fn push(&mut self, t: T) -> usize {
-        let association = self.association.iter_mut().find(|a| !a.full);
+        let association = self
+            .association
+            .iter_mut()
+            .find(|a| a.state == AssociationState::Hollow);
         if let Some(association) = association {
             // put data in free slot
             let bucket = self
@@ -93,7 +137,7 @@ impl<T> SVec<T> {
                                 .iter()
                                 .all(|&used_byte_tail| used_byte_tail == u8::MAX)
                             {
-                                association.full = true;
+                                association.state = AssociationState::Full;
                             }
                             return association.bucket_idx * BUCKET_SIZE
                                 + byte_idx * (u8::BITS as usize)
@@ -103,6 +147,19 @@ impl<T> SVec<T> {
                 }
             }
             panic!("there to be one non full byte, since the association said it was not full");
+        } else if let Some(association) = self
+            .association
+            .iter_mut()
+            .find(|a| a.state == AssociationState::Disabled)
+        {
+            let bucket_idx = self.data.len();
+            let mut bucket = Bucket::new();
+            bucket.data[0].write(t);
+            bucket.used[0] = 1u8;
+            self.data.push(bucket);
+            association.state = AssociationState::Hollow;
+            association.bucket_idx = bucket_idx;
+            bucket_idx * BUCKET_SIZE
         } else {
             // put data in new bucket
             let bucket_idx = self.data.len();
@@ -112,14 +169,93 @@ impl<T> SVec<T> {
             self.data.push(bucket);
             self.association.push(Association {
                 bucket_idx,
-                full: false,
+                state: AssociationState::Hollow,
             });
             bucket_idx * BUCKET_SIZE
         }
     }
 
     pub(crate) fn remove(&mut self, index: usize) -> Option<T> {
-        todo!()
+        let Some(association) = self.association.mut_valid(index / BUCKET_SIZE) else {println!("KUKUK"); return None};
+
+        let bucket = self.data.get_mut(association.bucket_idx)?;
+        let bucket_index = index % BUCKET_SIZE;
+        let bit: u8 = 1 << (bucket_index % (u8::BITS as usize));
+        let used_idx = bucket_index / (u8::BITS as usize);
+
+        let Some(used_byte) = bucket.used.get_mut(used_idx) else {println!("hmmm"); return None};
+
+        if (*used_byte & bit) == 0 {
+            println!("item non existent");
+            return None;
+        }
+
+        *used_byte &= !bit;
+        let t = bucket.data.get_mut(bucket_index).map(|t| {
+            let mut uninit = MaybeUninit::uninit();
+            std::mem::swap(t, &mut uninit);
+            unsafe { uninit.assume_init() }
+        });
+
+        // println!(
+        //     "index: {index}, buclet_index: {bucket_index}, used_byte: {}",
+        //     *used_byte
+        // );
+        association.state = if *used_byte == u8::MAX {
+            AssociationState::Full
+        } else {
+            AssociationState::Hollow
+        };
+        if bucket.used.iter().all(|used_byte| *used_byte == 0) {
+            // bucket free -> remove bucket
+
+            let last_idx = self.data.len() - 1;
+            let asso_bucket_idx = association.bucket_idx;
+            association.state = AssociationState::Disabled;
+            let _ = self.data.swap_remove(asso_bucket_idx);
+            self.association
+                .iter_mut()
+                .find(|asso| asso.bucket_idx == last_idx)
+                .unwrap()
+                .bucket_idx = asso_bucket_idx;
+        }
+
+        if t.is_none() {
+            println!("What");
+        }
+
+        t
+    }
+
+    fn print_meta_info(&self) {
+        println!("buckets: {}", self.data.len());
+        println!(
+            "associations: {}, {}",
+            self.association.len(),
+            self.association_state()
+        );
+        for b in &self.data {
+            for byte in b.used {
+                let str_repr: Vec<_> = (0usize..8)
+                    .map(|bit_idx| byte & (1u8 << bit_idx) != 0)
+                    .map(|set| if set { b'|' } else { b'_' })
+                    .collect();
+                let str_repr = String::from_utf8_lossy(&str_repr);
+                print!("{str_repr}");
+            }
+            println!("");
+        }
+    }
+
+    fn association_state(&self) -> String {
+        String::from_utf8_lossy(
+            &self
+                .association
+                .iter()
+                .map(|a| a.state.get_ascci_byte())
+                .collect::<Vec<_>>(),
+        )
+        .into_owned()
     }
 }
 
@@ -162,5 +298,23 @@ mod tests {
         for i in 0usize..1000 {
             assert_eq!(v.get(i).copied(), Some((i + 1) as i32));
         }
+    }
+
+    #[test]
+    fn push_and_remove() {
+        let mut v = SVec::new();
+        for i in 0..1000 {
+            v.push(i);
+        }
+        assert_eq!("FFFH", v.association_state());
+        for i in 0..1000 {
+            assert!(v.remove(i).is_some());
+        }
+        assert_eq!("DDDD", v.association_state());
+
+        for i in 0..1000 {
+            v.push(i);
+        }
+        assert_eq!("FFFH", v.association_state());
     }
 }
