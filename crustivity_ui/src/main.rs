1ea58a6d521049c19ae7258a6d39536dffccc689
N4tus <33029422+N4tus@users.noreply.github.com>
2023-07-24 14:46:44 +0200

    Add MPL notice to curstivity_ui files.

--- crustivity_ui/src/main.rs
+++ crustivity_ui/src/main.rs
@@ -1,4 +1,9 @@
 use crustivity_core::{constraints::ConstraintSystem, Effect, Event, Mut, Ref, WorldBuilder};
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 use crustivity_ui::interaction::{Init, MouseClick};
 use vello::{
     kurbo::Affine,
